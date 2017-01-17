c77527bc219226a6c2ea9996124e96b4258a4445
tranvuhoangduy <tranvhoangduy@outlook.com>
2017-01-17 09:21:29 +0000

    Diplay a message if no article

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -4,7 +4,7 @@ RSpec.feature "Listing Articles" do
  
  before do
    @article1 = Article.create(title:'The First article', body:'lorem ipsum dskdsd.')
-   @article2 = Article.create(title:'The second article',k body:'body of 2nd article')
+   @article2 = Article.create(title:'The second article', body:'body of 2nd article')
  end
  
  scenario "A user lists all articles" do
@@ -18,5 +18,24 @@ RSpec.feature "Listing Articles" do
    expect(page).to have_link(@article2.title)
 
  end
-  
+  scenario "A user has no articles" do
+   Article.delete_all
+
+   visit "/"
+
+   expect(page).not_to have_content(@article1.title)
+   expect(page).not_to have_content(@article1.body)
+   expect(page).not_to have_content(@article2.title)
+   expect(page).not_to have_content(@article2.body)
+   expect(page).not_to have_link(@article1.title)
+   expect(page).not_to have_link(@article2.title)
+
+   within("h1#no-articles") do
+     expect(page).to have_content("No Articles Created")
+   end
+
+
+ end
+
+
 end

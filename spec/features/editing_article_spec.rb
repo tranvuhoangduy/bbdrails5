035ae8e9db192bc46b9d6db6501017045c3a0da3
tranvuhoangduy <tranvhoangduy@outlook.com>
2017-01-17 09:37:39 +0000

    editing article

---
+++ spec/features/editing_article_spec.rb
@@ -0,0 +1,34 @@
+require 'rails_helper'
+
+RSpec.feature "Editing an article" do
+ before do
+  @article = Article.create(title:'Title one', body:'Body of article one')
+ end
+ scenario "A user updates an article" do
+  visit "/"
+  
+  click_link @article.title
+  click_link "Edit Article"
+
+  fill_in "Title" , with: "Updated Title"
+  fill_in "Body" , with: "Updated Body of Article"
+  click_button "Update Article"
+
+  expect(page).to have_content("Article has been updated")
+  expect(page.current_path).to eq(article_path(@article))
+ end
+
+ scenario "A user fails to updates an article" do
+  visit "/"
+  
+  click_link @article.title
+  click_link "Edit Article"
+
+  fill_in "Title" , with: ""
+  fill_in "Body" , with: "Updated Body of Article"
+  click_button "Update Article"
+
+  expect(page).to have_content("Article has not been updated")
+  expect(page.current_path).to eq(article_path(@article))
+ end
+end

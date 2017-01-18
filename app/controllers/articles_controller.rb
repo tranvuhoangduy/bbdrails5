56589de6ea2f11c3a018888b3ef142e9411c1db4
tranvuhoangduy <tranvhoangduy@outlook.com>
2017-01-18 06:27:38 +0000

    finish user devise

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,10 @@
 class ArticlesController < ApplicationController
  before_action :set_article , only: [:show,:edit,:update ,:destroy]
-
+ before_action :authenticate_user! , except: [:index, :show]
 
  def index
   @articles = Article.all
-end
+ end
 def new
  @article = Article.new
 end
@@ -25,16 +25,25 @@ def show
 end
 
 def edit
+unless @article.user == current_user
+flash[:alert] = "You can only edit your own article."
+redirect_to root_path 
+end
 end
 
 def update
-  if @article.update(article_params)
-    flash[:success] = "Article has been updated"
-    redirect_to @article
-  else
-    flash.now[:danger] = "Article has not been updated"
-    render :edit
-  end
+unless @article.user == current_user
+flash[:danger] = "You can only edit your own article."
+redirect_to root_path 
+else
+if @article.update(article_params) 
+flash[:success] = "Article has been updated" 
+redirect_to @article
+else
+flash.now[:danger] = "Article has not been updated" 
+render :edit
+end 
+end
 end
 
 def destroy
--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -33,6 +33,15 @@ RSpec.feature "Showing an article" do
    expect(page).not_to have_link("Delete Article")
    
   end
-  
+  scenario "A signed in owner sees both links" do 
+   login_as(@john)
+   visit "/"
+   click_link @article.title
+   expect(page).to have_content(@article.title) 
+   expect(page).to have_content(@article.body) 
+   expect(current_path).to eq(article_path(@article))
+   expect(page).to have_link("Edit Article")
+   expect(page).to have_link("Delete Article") 
+  end
   
 end
